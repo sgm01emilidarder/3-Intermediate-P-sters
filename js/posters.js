72a4a6ca516f211ff3b7c43bb6682cdc26a17d86
sgm01emilidarder <sgm01@iesemilidarder.com>
2019-10-24 20:48:28 +0200

    1

--- js/posters.js
+++ js/posters.js
@@ -1,4 +1,5 @@
-let content = document.querySelector('#peliculas');
+let content = document.querySelector('#peliculas2');
+let content2 = document.querySelector('#series2');
 
 function printCards(pList) {
   content.innerHTML = '';
@@ -9,9 +10,10 @@ function printCards(pList) {
       return actores;
     });
     content.innerHTML += `
+
             <div class="col-4 d-flex justify-content-center">
               <div class="card" style="width: 18rem;">
-                <img src=${list.imagen} class="card-img-top card-image" alt="...">
+                <img src="${list.imagen}" class="card-img-top card-image" alt="...">
                 <div class="card-body">
                   <h5 class="card-title d-flex justify-content-center">${list.titulo}</h5>
                   <p class="card-text d-flex justify-content-center">Duración: ${list.duracion}</p>
@@ -24,6 +26,8 @@ function printCards(pList) {
                   <ul class="list-group list-group-flush">
                     ${actores}
                   </ul>
+                <div class="card-body d-flex justify-content-center">
+                  <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pedido" onclick="myModal(${list.id}, '${list.titulo}', '${list.precio}')">Comprar</button>
                 </div>
               </div>
             </div>
@@ -32,17 +36,18 @@ function printCards(pList) {
 }
 
 function printCards2(sList) {
-  content.innerHTML = '';
+  content2.innerHTML = '';
   for (let list of sList.series) {
     let actores = "";
     list.actores.forEach(function(e) {
       actores += `<li class="list-group-item d-flex justify-content-center">${e.actor}</li>`;
       return actores;
     });
-    content.innerHTML += `
+    content2.innerHTML += `
+
             <div class="col-4 d-flex justify-content-center">
               <div class="card" style="width: 18rem;">
-                <img src=${list.imagen} class="card-img-top card-image" alt="...">
+                <img src="${list.imagen}" class="card-img-top card-image" alt="...">
                 <div class="card-body">
                   <h5 class="card-title d-flex justify-content-center">${list.titulo}</h5>
                   <p class="card-text d-flex justify-content-center">Temporadas: ${list.temporadas}</p>
@@ -52,12 +57,15 @@ function printCards2(sList) {
                 <div class="card-header d-flex justify-content-center">
                   Actores
                 </div>
-                  <ul class="list-group list-group-flush">
-                    ${actores}
-                  </ul>
-                </div>
+                <ul class="list-group list-group-flush">
+                  ${actores}
+                </ul>
+              <div class="card-body d-flex justify-content-center">
+                <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#pedido" onclick="myModal(${list.id}, '${list.titulo}', '${list.precio}')">Comprar</button>
               </div>
             </div>
+          </div>
+
         `
   }
 }
@@ -78,3 +86,9 @@ function getSeries() {
       printCards2(data);
     });
 }
+
+function myModal(id, nombre, precio) {
+  $("#ped_poster").val(nombre);
+  $("#ped_precio").val(precio);
+  $("#pedido").modal("show");
+};
